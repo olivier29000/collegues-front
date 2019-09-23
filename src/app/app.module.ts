d80f26085eb20b16cb0f19aa43229f0202edab92
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-09-23 12:22:06 +0200

    test bootstrap dynamique

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+
   imports: [
+    NgbModule,
     BrowserModule
   ],
   providers: [],
